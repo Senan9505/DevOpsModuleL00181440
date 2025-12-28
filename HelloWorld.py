9bb28cba81f10f0cbffda2eafadb70b7509d939f
Senan9505 <L00181440@atu.ie>
2025-12-28 23:09:22 +0000

    Refactor HelloWorld.py with functions and main check

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,10 @@
+def get_message() str:
+    return "Hello World"
 print("Hello World")
-
-if(1+6<6):
-    print("Yes")
-else:
-    print("No")
+def check_condition() str:
+    if(1+6<6):
+        return "Yes"
+    return "No"
+if __name__ == "__main__":
+    print(get_message())
+    print(check_condition())
