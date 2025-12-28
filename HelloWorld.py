01553e02be4cc43414d284bae4bb5b59f2c0359e
Senan9505 <L00181440@atu.ie>
2025-12-28 23:24:20 +0000

    Fix syntax issues in HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,11 @@
-def get_message() str:
+def get_message():
     return "Hello World"
 print("Hello World")
-def check_condition() str:
+def check_condition():
     if(1+6<6):
         return "Yes"
     return "No"
 if __name__ == "__main__":
     print(get_message())
     print(check_condition())
+
